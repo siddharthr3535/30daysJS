1cca05ed47859ae5a4426048bf2098d36cf52fc3
Sid <siddharthr3535@gmail.com>
2025-11-28 10:16:13 -0500

    makes more sense , await directly resolves and returns the result

--- addTwoPromises.js
+++ addTwoPromises.js
@@ -4,16 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {
-  const one = await promise1.then((value) => {
-    return value;
-  });
-  const two = await promise2.then((value) => {
-    return value;
-  });
+  const one = await promise1;
+  const two = await promise2;
   return one + two;
 };
 
-/**
- * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
- *   .then(console.log); // 4
- */
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
